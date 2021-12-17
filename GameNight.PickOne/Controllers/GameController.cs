ae013bb4e3bf590eb518ce624de40987bff2f3e4
JessePecar <JPecar19@gmail.com>
2021-12-17 13:07:50 -0500

    changed more status codes

--- GameNight.PickOne/Controllers/GameController.cs
+++ GameNight.PickOne/Controllers/GameController.cs
@@ -66,7 +66,7 @@ namespace GameNight.API.Controllers
             }
             catch(Exception ex)
             {
-                return BadRequest(ex.Message);
+                return StatusCode(500, ex.Message);
             }
         }
 
